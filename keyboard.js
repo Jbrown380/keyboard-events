39191770dff6d2c43587f6be59edbc3d712c13f5
Jaden Brown <jadenmb01@gmail.com>
2021-04-10 17:20:09 -0400

    initial commit

---
+++ keyboard.js
@@ -0,0 +1,10 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+const randomLetter = letters[Math.floor(Math.random() * 26)];
+
+const body = document.querySelector("body");
+const randomLetter = document.createElement("h3", alert("SECRET KEY PRESSED"));
+document.body.addEventListener("keypress", function(event) {
+const letterIndex = randomLetter.indexOf(event.key);
+const body = document.appendChild(randomLetter);
+});
